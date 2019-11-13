462b4641f444befff9e9b3d942ab68ad86792380
barbudan <dan.pltr@gmail.com>
2019-11-13 12:53:27 -0300

    Added UI, adding functionality and other features

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, ToastController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,8 +8,58 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class Page1 {
 
-  constructor(public navCtrl: NavController) {
+  tasks: Array<{ description: string, priority: string }> = [];
+  editDescricao: string = "";
+  editPrioridade: string = "";
 
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+  }
+
+  btnRemoveClick() {
+
+  }
+
+  btnAddClick() {
+    let priority;
+    priority = +this.editPrioridade;
+
+    if(this.editDescricao==="" || this.editPrioridade==="")
+    {
+      return; 
+    }
+
+    if(priority<1 || priority>10 )
+    {
+      let toast = this.toastCtrl.create({
+        message: "A prioridade deve estar entre 1 e 10.",
+        duration: 2000
+      });
+      toast.present();
+      return;
+    }    
+
+    for (let t of this.tasks) {
+      if(t.description === this.editDescricao)
+      {
+        let toast2 = this.toastCtrl.create({
+          message: "Tarefa já cadastrada.",
+          duration: 2000
+        });
+        toast2.present();
+        return;
+      }
+    }
+
+    let task = { description: this.editDescricao, priority: this.editPrioridade};
+    this.tasks.push(task);
+
+    this.tasks.sort((obj1, obj2) => {
+      if(obj1.priority<obj2.priority)
+      {
+        return -1;
+      }
+      return 1;
+    });
   }
 
 }
